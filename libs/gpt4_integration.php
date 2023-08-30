9e186bac20e7f6db41e327af816861d078966131
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-30 20:25:22 +0500

    Update gpt4_integration.php

--- libs/gpt4_integration.php
+++ libs/gpt4_integration.php
@@ -1,1 +1,32 @@
 <?php // GPT-4 API integration code ?>
+<?php
+// gpt4_integration.php
+
+// Placeholder for the GPT-4 API Key
+define('GPT4_API_KEY', 'YOUR_API_KEY_HERE');
+
+function getGPT4Response($prompt) {
+    $api_url = 'https://api.openai.com/v2/engines/text-davinci-003/completions'; 
+    $headers = array(
+        'Authorization: Bearer ' . GPT4_API_KEY,
+        'Content-Type: application/json',
+    );
+
+    $data = array(
+        'prompt' => $prompt,
+        'max_tokens' => 150
+    );
+
+    $ch = curl_init($api_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $decoded_response = json_decode($response, true);
+    return $decoded_response['choices'][0]['text'];
+}
+?>
