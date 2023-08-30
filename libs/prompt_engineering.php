8564defec9bb1a4fcf8154c0d602478a363024e4
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-30 20:23:58 +0500

    Update prompt_engineering.php

--- libs/prompt_engineering.php
+++ libs/prompt_engineering.php
@@ -1,1 +1,9 @@
 <?php // Enhance chatbot responses ?>
+<?php
+// prompt_engineering.php
+
+function enhancePrompt($prompt) {
+    // Placeholder: Modify the prompt based on custom data sources or logic.
+    return $prompt;
+}
+?>
